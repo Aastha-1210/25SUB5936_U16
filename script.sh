c9f9560ad6dd84f85ae3536cf6d2c802dc201424
Aastha-1210 <75885605+Aastha-1210@users.noreply.github.com>
2026-04-14 08:42:44 +0530

    Delete script.sh

--- script.sh
+++
@@ -1,8 +0,0 @@
-this file is generated using vim editor.
-Linux is an Operating System
-Linux is an Operating System
-Linux is an Operating System
-this file is generated using vim editor.
-this file is generated using vim editor.
-Linux is an Operating System
-Linux is an Operating System
